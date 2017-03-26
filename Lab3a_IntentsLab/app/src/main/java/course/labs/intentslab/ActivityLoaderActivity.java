acd6c675f7697a658374681416cfdeea5a537261
alangiovannipc <alangiovannipc@gmail.com>
2017-03-25 23:56:29 -0500

    [Lab3a_IntentsLab] ImplicitIntent Created a base intent for viewing a URl,Created a chooser intent for choosing which activity and started the chooser activity

--- Lab3a_IntentsLab/app/src/main/java/course/labs/intentslab/ActivityLoaderActivity.java
+++ Lab3a_IntentsLab/app/src/main/java/course/labs/intentslab/ActivityLoaderActivity.java
@@ -83,19 +83,19 @@ public class ActivityLoaderActivity extends Activity {
 		// TODO - Create a base intent for viewing a URL
 		// (HINT:  second parameter uses Uri.parse())
 		
-        Intent baseIntent = null;
+        Intent baseIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("www.google.com.pe"));
 		
 		// TODO - Create a chooser intent, for choosing which Activity
 		// will carry out the baseIntent
 		// (HINT: Use the Intent class' createChooser() method)
-		Intent chooserIntent = null;
+		Intent chooserIntent = Intent.createChooser(baseIntent,"Choose the browser");
         
         
 		Log.i(TAG,"Chooser Intent Action:" + chooserIntent.getAction());
         
         
 		// TODO - Start the chooser Activity, using the chooser intent
-
+		startActivity(chooserIntent);
         
 	}
     
